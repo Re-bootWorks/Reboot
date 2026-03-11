cba9b452b1e5d3ee06fd570d5168119c10241f7e
celine <wisohyun0@gmail.com>
2026-03-11 17:51:19 +0900

    feat(ui): empty 컴포넌트 추가
    - 빈페이지를 위한 empty 컴포넌트 타입 2개 추가
    - 모든 페이지 배경 색상을 gray-50으로 변경
    
    Resolves: #2

---
+++ components/layout/Empty/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import ImgEmpty from "@/assets/img/img_empty.svg";
+import { ReactNode } from "react";
+import { cn } from "@/utils/cn";
+
+interface EmptyProps {
+	section?: boolean;
+	className?: string;
+	children: ReactNode;
+}
+const pageStyle = "h-full justify-center";
+const sectionStyle = "rounded-4xl bg-white ";
+export default function Empty({ section = false, className, children }: EmptyProps) {
+	return (
+		<section
+			className={cn(
+				"md:py-14. flex flex-col px-6 py-10",
+				section ? sectionStyle : pageStyle,
+				className,
+			)}>
+			<div>
+				<Image
+					src={ImgEmpty}
+					alt="컨텐츠가 없습니다."
+					width={120}
+					height={72}
+					className="mx-auto"
+				/>
+				<div className="mt-5 text-center text-sm text-gray-500 md:mt-6 md:text-base">
+					{children}
+				</div>
+			</div>
+		</section>
+	);
+}
