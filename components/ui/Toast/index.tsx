8a252488da0145640a10eea982cbc97f13d13f80
celine <wisohyun0@gmail.com>
2026-03-13 14:10:47 +0900

    design(toast): 애니메이션 추가 및 반응형에 따른  toast 위치 수정
    Resolves: #12

--- components/ui/Toast/index.tsx
+++ components/ui/Toast/index.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/utils/cn";
 import { ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { Status, ToastBoxProps } from "./type";
+import { AnimatePresence, motion } from "motion/react";
 
 interface ToastBoxDisplayProps {
 	status?: Status;
@@ -11,13 +12,21 @@ interface ToastBoxDisplayProps {
 
 export function ToastBox({ status, children }: ToastBoxDisplayProps) {
 	return (
-		<div
+		<motion.div
+			layout
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: -20 }}
+			transition={{
+				duration: 0.22,
+				layout: { duration: 0.2 },
+			}}
 			className={cn(
 				!status ? "bg-black-70" : status === "success" ? "bg-green-500" : "bg-error",
 				"w-fit rounded-[0.625rem] px-6 py-3 text-center text-xs font-semibold break-keep text-white md:rounded-xl md:px-8 md:py-4 md:text-base",
 			)}>
 			{children}
-		</div>
+		</motion.div>
 	);
 }
 
@@ -31,15 +40,17 @@ export default function Toast({ toasts }: { toasts: ToastBoxProps[] }) {
 	if (!mounted) return null;
 
 	return createPortal(
-		<div className="fixed bottom-24 left-1/2 z-10 flex -translate-x-1/2 flex-col items-center gap-4">
-			{toasts.map((toast) => {
-				const { id, status, message } = toast;
-				return (
-					<ToastBox key={id} status={status}>
-						{message}
-					</ToastBox>
-				);
-			})}
+		<div className="fixed bottom-10 left-1/2 z-10 flex -translate-x-1/2 flex-col items-center gap-4 lg:bottom-24">
+			<AnimatePresence initial={false} mode="popLayout">
+				{toasts.map((toast) => {
+					const { id, status, message } = toast;
+					return (
+						<ToastBox key={id} status={status}>
+							{message}
+						</ToastBox>
+					);
+				})}
+			</AnimatePresence>
 		</div>,
 		document.body,
 	);
