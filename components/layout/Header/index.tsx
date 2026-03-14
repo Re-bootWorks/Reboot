fa6c8b9650bb5e694fbaed380872403e25ac0c58
celine <wisohyun0@gmail.com>
2026-03-14 17:15:12 +0900

    design(GNB): gNB 및 Sidebar 구현
    - navigation 상수파일 생성
    - 전역 layout에 컴포넌트 추가
    - 유저 기본 이미지 asset 추가
    
    Resolves: #22

---
+++ components/layout/Header/index.tsx
@@ -0,0 +1,170 @@
+"use client";
+
+import Link from "next/link";
+import Container from "../Container";
+import { ACCOUNT_MENU_ITEM, GNB_MENU_ITEMS } from "@/constants/navigation";
+import { ReactNode } from "react";
+import { cn } from "@/utils/cn";
+import {
+	IcBellOutline,
+	IcBellUnreadOutline,
+	IcChevronRight,
+	IcDelete,
+	IcMenu,
+} from "@/components/ui/icons";
+import Image from "next/image";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import useToggle from "@/hooks/use-toggle";
+
+interface GNBProps {
+	favoritesCount: number;
+	showChevron?: boolean;
+	handleItemClick?: () => void;
+	className?: string;
+}
+interface SidebarProps {
+	isOpen: boolean;
+	isLogin: boolean;
+	handleSidebarClose: () => void;
+	favoritesCount: number;
+}
+
+const STYLE = {
+	header: "sticky top-0 left-0 z-10 flex items-center justify-between bg-gray-50 py-2 md:py-4 ",
+	logo: "font-taenada w-fit p-2 text-base leading-none text-purple-500 md:text-2xl",
+	gnb: "flex items-center gap-4",
+	sidebar:
+		"fixed inset-0 flex h-full max-w-xl w-11/12 flex-col justify-between rounded-r-3xl bg-white py-6",
+	sidebarItem: "px-4 py-2 flex items-center justify-between",
+	link: "flex items-center gap-1 p-4 text-base text-gray-600",
+	etc: "flex items-center gap-4 md:gap-6",
+	image: "rounded-full border border-gray-200",
+};
+
+function GNBBadge({ children }: { children: ReactNode }) {
+	return (
+		<span className="rounded-full bg-purple-500 px-2 text-xs font-semibold text-white">
+			{children}
+		</span>
+	);
+}
+
+function GNB({ favoritesCount, handleItemClick, showChevron = false, className }: GNBProps) {
+	return (
+		<>
+			{GNB_MENU_ITEMS.map((item) => {
+				const { href, label, key } = item;
+				return (
+					<Link key={key} href={href} className={className} onClick={handleItemClick}>
+						<span className={STYLE.link}>
+							{label}
+							{key === "favorites" && <GNBBadge>{favoritesCount}</GNBBadge>}
+						</span>
+						{showChevron && <IcChevronRight />}
+					</Link>
+				);
+			})}
+		</>
+	);
+}
+
+function Sidebar({ isOpen, isLogin, handleSidebarClose, favoritesCount }: SidebarProps) {
+	return (
+		<Dialog open={isOpen} onClose={handleSidebarClose} className={"relative z-30 md:hidden"}>
+			<DialogBackdrop className="bg-black-50 fixed inset-0" />
+			<DialogPanel className={STYLE.sidebar}>
+				<div>
+					<button
+						type="button"
+						onClick={handleSidebarClose}
+						className="mb-6 ml-6 cursor-pointer"
+						aria-label="모바일 내비게이션 닫기">
+						<IcDelete />
+					</button>
+					<nav aria-label="모바일 내비게이션">
+						<GNB
+							showChevron
+							favoritesCount={favoritesCount}
+							handleItemClick={handleSidebarClose}
+							className={STYLE.sidebarItem}
+						/>
+						{isLogin && (
+							<Link
+								href={ACCOUNT_MENU_ITEM.href}
+								className={STYLE.sidebarItem}
+								onClick={handleSidebarClose}>
+								<span className={STYLE.link}>{ACCOUNT_MENU_ITEM.label}</span>
+								<IcChevronRight />
+							</Link>
+						)}
+					</nav>
+				</div>
+				{isLogin ? (
+					<button className={cn(STYLE.sidebarItem, "justify-end")}>
+						<span className={STYLE.link}>로그아웃</span>
+					</button>
+				) : (
+					<button className={cn(STYLE.sidebarItem, "justify-end")}>
+						<span className={STYLE.link}>로그인</span>
+					</button>
+				)}
+			</DialogPanel>
+		</Dialog>
+	);
+}
+
+export default function Header() {
+	const { isOpen, setOpen, setClose } = useToggle();
+	// @TODO 추후 기능 구현 예정
+	const isLogin = true;
+	const isAlarm = false;
+	const favoritesCount = 1;
+	const profile = null;
+
+	return (
+		<Container as="header" className={STYLE.header}>
+			<div className={STYLE.gnb}>
+				<Link href="/">
+					<h1 className={STYLE.logo}>Re:boot</h1>
+				</Link>
+				<nav className="hidden md:flex" aria-label="상단 내비게이션">
+					<GNB favoritesCount={favoritesCount} />
+				</nav>
+				<Sidebar
+					isOpen={isOpen}
+					isLogin={isLogin}
+					handleSidebarClose={setClose}
+					favoritesCount={favoritesCount}
+				/>
+			</div>
+			<div className={STYLE.etc}>
+				{isLogin ? (
+					<>
+						<button type="button" className={cn(isAlarm ? "cursor-pointer" : "cursor-default")}>
+							{isAlarm ? <IcBellUnreadOutline /> : <IcBellOutline />}
+						</button>
+						<div className="hidden md:block">
+							<button type="button" className={STYLE.image}>
+								<Image
+									src={profile ? profile : "/assets/img/img_profile.svg"}
+									alt="프로필 이미지"
+									width={44}
+									height={44}
+								/>
+							</button>
+						</div>
+					</>
+				) : (
+					<>
+						<button type="button" className={cn(STYLE.link, "hidden md:block")}>
+							로그인
+						</button>
+					</>
+				)}
+				<button type="button" className="cursor-pointer md:hidden" onClick={setOpen}>
+					<IcMenu />
+				</button>
+			</div>
+		</Container>
+	);
+}
---
+++ constants/navigation.ts
@@ -0,0 +1,24 @@
+export const GNB_MENU_ITEMS = [
+	{
+		href: "#",
+		label: "모임 찾기",
+		key: "meetings",
+	},
+	{
+		href: "#",
+		label: "찜한 모임",
+		key: "favorites",
+	},
+	{
+		href: "#",
+		label: "모든 리뷰",
+		key: "reviews",
+	},
+	{
+		href: "#",
+		label: "달램 토크",
+		key: "posts",
+	},
+];
+
+export const ACCOUNT_MENU_ITEM = { href: "#", label: "마이페이지", key: "mypage" };
