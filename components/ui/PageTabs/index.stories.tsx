4ec680f4d499f8d970de2faca1985d7465863544
PollyGotACracker <bitterns96@gmail.com>
2026-03-18 16:57:24 +0900

    refactor(ui): pageTabs, loaderDots storybook 수정

--- components/ui/PageTabs/index.stories.tsx
+++ components/ui/PageTabs/index.stories.tsx
@@ -5,6 +5,9 @@ import { IcAlignLeft, IcAlignRight } from "../icons";
 const meta: Meta<typeof PageTabs> = {
 	title: "UI/PageTabs",
 	component: PageTabs,
+	subcomponents: {
+		Item: PageTabs.Item,
+	},
 	tags: ["autodocs"],
 	parameters: {
 		layout: "padded",
@@ -26,13 +29,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	parameters: {
-		docs: {
-			description: {
-				story: "data, defaultId, onChange 설정으로 페이지 탭 컴포넌트 구성",
-			},
-		},
-	},
 	render: (args) => (
 		<PageTabs defaultId="left" {...args}>
 			<PageTabs.Item id="left" icon={<IcAlignLeft />}>
