742ea05f6d7f80817bac1f3415a56bb076512604
Geunwon Park <rmsdnjsaos@gmail.com>
2026-03-19 14:15:49 +0900

    design(compactcard): 불필요한 요소 제거 및 일부 속성 props 전환
    Resolves: #70

--- features/Post/components/CompactCard/index.tsx
+++ features/Post/components/CompactCard/index.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/features/Post/utills";
 import { TimeTag } from "@/components/ui/Tags/TimeTag";
 import { IcLocation } from "@/components/ui/icons";
-import { useState } from "react";
 
 interface CompactCardProps {
 	registrationEnd: string;
@@ -18,6 +17,8 @@ interface CompactCardProps {
 	name: string;
 	type: string;
 	region: string;
+	isPressed: boolean;
+	onPress: () => void;
 }
 
 export default function CompactCard({
@@ -26,53 +27,47 @@ export default function CompactCard({
 	name,
 	type,
 	region,
+	isPressed,
+	onPress,
 }: CompactCardProps) {
 	const isClosed = isDeadlinePassed(registrationEnd);
-	const [isPressed, setIsPressed] = useState(false);
 	return (
-		<>
-			<div className="flex h-67.5 w-40.5 flex-col gap-2.5 md:h-71.5 md:w-75.5 md:gap-3.5">
-				<div className="relative h-40.5 w-40.5 overflow-hidden rounded-2xl md:h-45 md:w-75.5 md:rounded-3xl">
-					<Image
-						alt={"defaultImage"}
-						src="./assets/img/img_empty.svg"
-						fill
-						className="object-contain"
-					/>
-					<UtilityButton
-						sizes="small"
-						pressed={isPressed}
-						onClick={() => setIsPressed((prev) => !prev)}
-						className="absolute right-3.5 bottom-3.5 md:right-5 md:bottom-5 md:h-12 md:w-12"
-					/>
-				</div>
-				<div className="flex h-24.5 w-40.5 flex-col gap-1.5 p-1 md:h-23 md:w-75.5 md:gap-4">
-					<div className="flex h-12.5 w-28.25 flex-col gap-1.5 md:flex-row">
-						{!isClosed && (
-							<DeadlineTag size="sm" className="md:h-6 md:text-sm">
-								{uiFormatDeadline(registrationEnd)}
-							</DeadlineTag>
-						)}
-						<div className="flex gap-2.5 md:gap-2">
-							<TimeTag size="sm" className="md:h-6 md:text-sm">
-								{uiFormatDate(dateTime)}
-							</TimeTag>
-							<TimeTag size="sm" className="md:h-6 md:text-sm">
-								{uiFormatTime(dateTime)}
-							</TimeTag>
-						</div>
+		<div className="flex h-67.5 w-40.5 flex-col gap-2.5 md:h-71.5 md:w-75.5 md:gap-3.5">
+			<div className="relative h-40.5 w-40.5 overflow-hidden rounded-2xl md:h-45 md:w-75.5 md:rounded-3xl">
+				<Image alt={"모임 이미지"} src="./assets/img/img_empty.svg" fill className="object-cover" />
+				<UtilityButton
+					sizes="small"
+					pressed={isPressed}
+					onClick={onPress}
+					className="absolute right-3.5 bottom-3.5 md:right-5 md:bottom-5 md:h-12 md:w-12"
+				/>
+			</div>
+			<div className="flex h-24.5 w-40.5 flex-col gap-1.5 p-1 md:h-23 md:w-75.5 md:gap-4">
+				<div className="flex h-12.5 w-28.25 flex-col gap-1.5 md:flex-row">
+					{!isClosed && (
+						<DeadlineTag size="sm" className="md:h-6 md:text-sm">
+							{uiFormatDeadline(registrationEnd)}
+						</DeadlineTag>
+					)}
+					<div className="flex gap-2.5 md:gap-2">
+						<TimeTag size="sm" className="md:h-6 md:text-sm">
+							{uiFormatDate(dateTime)}
+						</TimeTag>
+						<TimeTag size="sm" className="md:h-6 md:text-sm">
+							{uiFormatTime(dateTime)}
+						</TimeTag>
 					</div>
-					<div className="flex h-10.5 w-38.5 flex-col gap-0.5 md:h-13 md:w-73.5">
-						<h2 className="truncate text-base font-semibold md:text-xl">{name}</h2>
-						<div className="flex items-center gap-0.5">
-							<IcLocation size="xxs" className="md:h-4 md:w-4" />
-							<span className="text-xs font-medium text-gray-600 md:text-sm">
-								{region}·{type}
-							</span>
-						</div>
+				</div>
+				<div className="flex h-10.5 w-38.5 flex-col gap-0.5 md:h-13 md:w-73.5">
+					<h2 className="truncate text-base font-semibold md:text-xl">{name}</h2>
+					<div className="flex items-center gap-0.5">
+						<IcLocation size="xxs" className="md:h-4 md:w-4" />
+						<span className="text-xs font-medium text-gray-600 md:text-sm">
+							{region}·{type}
+						</span>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
