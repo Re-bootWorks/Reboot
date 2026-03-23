cf3c7872726272502ab073eb69ac00880d247c00
PollyGotACracker <bitterns96@gmail.com>
2026-03-24 01:04:32 +0900

    config(ci): ui 테스트 설정 및 패키지 추가

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,14 @@
+import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
-const createJestConfig = nextJest({ dir: "./" });
-
-export default createJestConfig({
-	testEnvironment: "node",
+const createJestConfig = nextJest({
+	dir: "./",
 });
+
+const config: Config = {
+	coverageProvider: "v8",
+	testEnvironment: "jsdom",
+	setupFilesAfterEnv: ["./jest.setup.ts"],
+};
+
+export default createJestConfig(config);
