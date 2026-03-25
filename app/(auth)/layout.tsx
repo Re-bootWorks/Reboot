83815813a649d5785a261bcb885cee3592a3209d
songtaeha <songtaeha@naver.com>
2026-03-25 16:43:40 +0900

    refactor(auth): authFormContainer 추가 및 적용

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="flex min-h-[calc(100vh-48px)] min-h-[calc(100vh-88px)] flex-col items-center justify-center bg-gray-50">
+		<div className="flex min-h-[calc(100vh-48px)] flex-col items-center justify-center bg-gray-50 md:min-h-[calc(100vh-88px)]">
 			{children}
 		</div>
 	);
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,22 +3,23 @@
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { LoginForm } from "@/features/auth/components/LoginModal/LoginForm";
+import AuthFormContainer from "@/features/auth/components/AuthFormContainer";
 
 export default function LoginPage() {
 	const router = useRouter();
 
 	return (
-		<div>
-			<div className="w-[343px] rounded-3xl bg-white p-6 shadow-xl md:w-142 md:px-14 md:pt-12 md:pb-11">
-				<h2 className="mb-8 text-center text-2xl font-semibold text-gray-900 md:mb-12">로그인</h2>
-				<LoginForm onSuccess={() => router.push("/")} />
-				<p className="mt-8 text-center text-sm font-medium text-gray-800">
+		<AuthFormContainer
+			title="로그인"
+			footer={
+				<>
 					같이달램이 처음이신가요?{" "}
 					<Link href="/signup" className="font-semibold text-purple-600 underline">
 						회원가입
 					</Link>
-				</p>
-			</div>
-		</div>
+				</>
+			}>
+			<LoginForm onSuccess={() => router.push("/")} />
+		</AuthFormContainer>
 	);
 }
