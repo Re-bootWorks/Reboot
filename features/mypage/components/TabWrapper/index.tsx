86e50e4d1972880e789f77091911cf2f448d5a8e
celine <wisohyun0@gmail.com>
2026-03-28 19:00:00 +0900

    refactor(mypage): useQueryParams hook 사용하여 탭 연결
    Resolves: #123

--- features/mypage/components/TabWrapper/index.tsx
+++ features/mypage/components/TabWrapper/index.tsx
@@ -1,13 +1,14 @@
 "use client";
-import { useState } from "react";
 import PageTabs from "@/components/ui/PageTabs";
 import { TabId } from "@/features/mypage/types";
 import Meetup from "../Meetup";
 import Review from "../Review";
 import Created from "../Created";
+import { useQueryParams } from "@/hooks/useQueryParams";
 
 export default function TabWrapper() {
-	const [activeTab, setActiveTab] = useState<TabId>("meetup");
+	const { get, set } = useQueryParams();
+	const activeTab = (get("tab") as TabId) ?? "meetup";
 
 	const tabContents: Record<TabId, React.ReactNode> = {
 		meetup: <Meetup />,
@@ -17,7 +18,7 @@ export default function TabWrapper() {
 
 	return (
 		<div className="min-w-0 grow">
-			<PageTabs defaultId={"meetup"} onChange={({ id }) => setActiveTab(id as TabId)}>
+			<PageTabs defaultId={activeTab} onChange={({ id }) => set({ tab: id })}>
 				<PageTabs.Item id="meetup">나의 모임</PageTabs.Item>
 				<PageTabs.Item id="review">나의 리뷰</PageTabs.Item>
 				<PageTabs.Item id="created">내가 만든 모임</PageTabs.Item>
