cd579fe7c413f77f92313d2310f6402e50bf9385
songtaeha <songtaeha@naver.com>
2026-03-31 15:51:13 +0900

    design: auth 컴포넌트 넓이 수정
    resolves: #175

--- features/auth/components/SignUpModal/index.tsx
+++ features/auth/components/SignUpModal/index.tsx
@@ -32,7 +32,7 @@ export function SignUpModal({ hideCloseButton }: SignUpModalProps) {
 				</div>
 			}
 			footerClassName="md:pt-10 text-center text-sm font-medium text-gray-800 md:text-[15px] md:leading-normal"
-			className="max-h-[90vh] w-[343px] p-6 md:w-142 md:px-14 md:py-10">
+			className="max-h-[90vh] max-w-[343px] p-6 md:max-w-142 md:px-14 md:py-10">
 			<SignUpForm onSuccess={closeSignup} />
 		</Modal>
 	);
