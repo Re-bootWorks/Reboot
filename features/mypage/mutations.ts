710222dcbd9d3282e79bb80b65f91f20fb420ba3
celine <wisohyun0@gmail.com>
2026-04-03 13:31:14 +0900

    fix(notifications): 알림 쿼리키 추가
    Resolves: #187

--- features/mypage/mutations.ts
+++ features/mypage/mutations.ts
@@ -82,7 +82,7 @@ export function usePatchMeetingsStatus() {
 				message: `모임이 ${isConfirmed ? "확정" : "취소"}되었습니다.`,
 				status: "success",
 			});
-			queryClient.invalidateQueries({ queryKey: headerQueryKeys.notifications });
+			queryClient.invalidateQueries({ queryKey: headerQueryKeys.all });
 			queryClient.invalidateQueries({ queryKey: mypageQueryKeys.meetups });
 			queryClient.invalidateQueries({ queryKey: mypageQueryKeys.created });
 			queryClient.invalidateQueries({
@@ -114,7 +114,7 @@ export function useDeleteMeetings() {
 				message: "모임이 삭제 되었습니다.",
 				status: "success",
 			});
-			queryClient.invalidateQueries({ queryKey: headerQueryKeys.notifications });
+			queryClient.invalidateQueries({ queryKey: headerQueryKeys.all });
 			queryClient.invalidateQueries({ queryKey: mypageQueryKeys.meetups });
 			queryClient.invalidateQueries({ queryKey: mypageQueryKeys.created });
 			queryClient.invalidateQueries({
@@ -145,7 +145,7 @@ export function useDeleteMeetingsJoin() {
 				message: "모임 예약이 취소 되었습니다.",
 				status: "success",
 			});
-			queryClient.invalidateQueries({ queryKey: headerQueryKeys.notifications });
+			queryClient.invalidateQueries({ queryKey: headerQueryKeys.all });
 			queryClient.invalidateQueries({ queryKey: mypageQueryKeys.meetups });
 			queryClient.invalidateQueries({ queryKey: mypageQueryKeys.created });
 			queryClient.invalidateQueries({
