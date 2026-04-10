1bcebac384e3b0b94fbd0edff9eb79e98d2704c7
김재희 <heedev94@gmail.com>
2026-04-10 15:59:57 +0900

    refactor(config): getQueryClient 커스텀 함수 추가
    - SSR 환경에서 클라이언트 측 불필요한 refetch를 방지하기 위해 staleTime 설정
    - environmentManager는 TanStack Query의 내부 API로 직접 사용이 권장되지 않아, 버전 업그레이드 시 예기치 않은 오류가 발생할 수 있음
    따라서 deprecated된 isServer 대신 Next.js 환경에서 보다 표준적이고 안전한 typeof window === 'undefined' 방식으로 환경 체크 로직 변경
    
    Resolves: #268

--- libs/getQueryClient.ts
+++ libs/getQueryClient.ts
@@ -1,15 +1,22 @@
-import { environmentManager, QueryClient } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 let browserQueryClient: QueryClient | undefined = undefined;
 
+function makeQueryClient() {
+	return new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: 60 * 1000,
+			},
+		},
+	});
+}
+
 export function getQueryClient() {
-	if (environmentManager.isServer()) {
-		return new QueryClient();
+	if (typeof window === "undefined") {
+		return makeQueryClient();
+	} else {
+		if (!browserQueryClient) browserQueryClient = makeQueryClient();
+		return browserQueryClient;
 	}
-
-	if (!browserQueryClient) {
-		browserQueryClient = new QueryClient();
-	}
-
-	return browserQueryClient;
 }
