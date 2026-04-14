df4f32dc052c563ac65f0d57541fb601c6865718
celine <wisohyun0@gmail.com>
2026-04-15 00:54:28 +0900

    design(ui/ErrorPage): errorpage className 추가 및 스타일 수정
    Resolves: #295

--- components/common/ErrorPage/index.tsx
+++ components/common/ErrorPage/index.tsx
@@ -2,12 +2,14 @@
 
 import Button from "@/components/ui/Buttons/Button";
 import Empty from "@/components/ui/Empty";
+import { cn } from "@/utils/cn";
 
 type ErrorPageProps = {
 	onRetryAction: () => void;
 	prefix?: string;
 	title?: string;
 	description?: string;
+	className?: string;
 };
 
 export default function ErrorPage({
@@ -15,14 +17,15 @@ export default function ErrorPage({
 	prefix,
 	title = "불러오지 못했습니다.",
 	description = "잠시 후 다시 시도해주세요. \n 문제가 계속되면 새로고침 후 다시 확인해주세요.",
+	className,
 }: ErrorPageProps) {
 	return (
-		<Empty>
-			<p>
+		<Empty className={cn("min-h-[50vh] w-full", className)}>
+			<p className="mb-4 text-lg font-semibold text-gray-800">
 				{prefix}
 				{title}
 			</p>
-			<p>{description}</p>
+			<p className="whitespace-pre-line">{description}</p>
 			<Button sizes="small" className="mx-auto mt-4 w-30" onClick={onRetryAction}>
 				다시 시도
 			</Button>
