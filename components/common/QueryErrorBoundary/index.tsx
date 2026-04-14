2da4b20bb2df6a82ca0426c0af882fb1f449de67
celine <wisohyun0@gmail.com>
2026-04-14 14:04:00 +0900

    refactor(ui/QueryErrorBoundary): description props 추가
    Resolves: #267

--- components/common/QueryErrorBoundary/index.tsx
+++ components/common/QueryErrorBoundary/index.tsx
@@ -9,15 +9,22 @@ import Button from "@/components/ui/Buttons/Button";
 interface ErrorFallbackProps extends FallbackProps {
 	prefix?: string;
 	title?: string;
+	description?: string;
 }
 
 interface QueryErrorBoundaryProps {
 	prefix: string;
 	title?: string;
 	children: ReactNode;
+	description?: string;
 }
 
-function ErrorFallback({ resetErrorBoundary, prefix, title }: ErrorFallbackProps) {
+function ErrorFallback({
+	resetErrorBoundary,
+	prefix,
+	title = "불러오지 못했습니다.",
+	description = "잠시 후 다시 시도해주세요.",
+}: ErrorFallbackProps) {
 	return (
 		<>
 			<Empty>
@@ -25,7 +32,7 @@ function ErrorFallback({ resetErrorBoundary, prefix, title }: ErrorFallbackProps
 					{prefix}
 					{title}
 				</p>
-				<p>잠시 후 다시 시도해주세요.</p>
+				<p>{description}</p>
 				<Button sizes="small" className="mx-auto mt-4 w-30" onClick={resetErrorBoundary}>
 					다시 시도
 				</Button>
@@ -36,7 +43,8 @@ function ErrorFallback({ resetErrorBoundary, prefix, title }: ErrorFallbackProps
 
 export default function QueryErrorBoundary({
 	prefix,
-	title = "불러오지 못했습니다.",
+	title,
+	description,
 	children,
 }: QueryErrorBoundaryProps) {
 	return (
@@ -44,7 +52,9 @@ export default function QueryErrorBoundary({
 			{({ reset }) => (
 				<ErrorBoundary
 					onReset={reset}
-					FallbackComponent={(props) => <ErrorFallback {...props} prefix={prefix} title={title} />}>
+					FallbackComponent={(props) => (
+						<ErrorFallback {...props} prefix={prefix} title={title} description={description} />
+					)}>
 					{children}
 				</ErrorBoundary>
 			)}
